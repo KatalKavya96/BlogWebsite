6f14f5dd9da99d443be06bbdf256f271638c5a18
Kavya Katal <kavyakatal09@gmail.com>
2025-03-31 02:25:07 +0530

    Commit 33

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -14,7 +14,7 @@ const Slider = ({ setIsAuthenticated }) => {
 
       <svg onClick={() => setIsOpen(!isOpen)} className={`fixed top-28 z-50 w-9 h-9 bg-black/70 cursor-pointer p-1 rounded-full transition-all duration-300 ${isOpen ? 'left-2' : 'left-85.5'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white"><path d="M12 11V8L16 12L12 16V13H8V11H12Z" /><circle cx="12" cy="12" r="10" fill="none" stroke="white" strokeWidth="2" /></svg>
     
-        <div className={`fixed top-0 left-0 h-[75vh] mt-[64px] overflow-y-scroll  border-b border-white/20 border-r border-white/20 transition-all duration-300 ease-in-out bg-adder shadow-[5px_10px_15px_2px_rgba(0,0,0,0.5)]    ${isOpen ? 'w-0' : 'w-90 overflow-hidden custom-scroll-hide'}`}>
+        <div className={`fixed top-0 left-0 h-[70vh] mt-[64px] overflow-y-scroll  border-b border-white/20 border-r border-white/20 transition-all duration-300 ease-in-out bg-adder shadow-[5px_10px_15px_2px_rgba(0,0,0,0.5)]    ${isOpen ? 'w-0' : 'w-90 overflow-hidden custom-scroll-hide'}`}>
 
           <div className="flex flex-col text-white gap-6 p-6">
 
